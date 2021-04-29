e68512b194f0e6832bd4fad4d9c02bad3b19bb74
Ivan Kucherenko <stysha.72@mail.ru>
2021-04-30 02:30:37 +0500

    analytics

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <div>
-        analytics
+        <a href="https://metrika.yandex.ru/stat/?id=77014948&amp;from=informer" target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/77014948/3_1_FFFFFFFF_EFEFEFFF_0_pageviews" style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="77014948" data-lang="ru" /></a>
       </div>
       <NowOnline />
     </div>
